cf8b8f271d86e1b2a7444e57bf37b7e6bf15fcf8
CauaRodriguesS <caua20005@gmail.com>
2023-11-05 00:31:48 -0300

    a

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-addEventListener("scroll",function(){
-    let nav = document.querySelector('nav')
-    nav.classList.toggle('rolagem',window,scrollY > 0)
+let nav = document.querySelector('#na')
+function inicio(){
+    nav.style.backgroundImage = "linear-gradient = (to bottom, #261435, #150D1B)"
+}
+window.addEventListener("scroll",function(){
+    if(scrollY > 0 ){
+        nav.style.backgroundImage = "#1a191bbe"
+    }else{
+        nav.style.backgroundImage = "linear-gradient = (to bottom, #261435, #150D1B)"
+    }
 })
