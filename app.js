bc705ffd71a6771295b74b23d2136db46eef3c85
CauaRodriguesS <caua20005@gmail.com>
2023-11-16 12:30:50 -0300

    a

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ function inicio(){
 }
 window.addEventListener("scroll",() => {
     if(scrollY > 0 ){
-        nav.style.backgroundColor =  "#1a191bbe"
+        nav.style.backgroundColor =  "#26143570"
         nav.style.backgroundImage = ""
     }else{
-        alert("foi")
         nav.style.backgroundColor= ""
         nav.style.backgroundImage = "linear-gradient(to bottom, #261435, #150D1B)"
     }
