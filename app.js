d154aa28a794c075e707672bb5374867ce90492c
Cauã Rodrigues <138609422+CauaRodriguesS@users.noreply.github.com>
2023-11-16 09:51:50 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let nav = document.querySelector('#na')
 function inicio(){
-    nav.style.backgroundImage = "linear-gradient = (to bottom, #261435, #150D1B)"
+    nav.style.backgroundImage = "linear-gradient(to bottom, #261435, #150D1B)"
 }
 window.addEventListener("scroll",function(){
     if(scrollY > 0 ){
